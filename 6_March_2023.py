fd804788c3f02220074e387158d86cbde09e7dd1
Sheng <cyusheng93@gmail.com>
2023-03-07 10:25:26 +0000

    Committeed first challenge. Also incldued one pass solution for this puzzle

---
+++ 6_March_2023.py
@@ -0,0 +1,25 @@
+numbers = [10, 15, 3, 2, 18, 20, 100, 105, 300, 500, 6, 1]
+k = 17
+
+# multiple passes
+def evaluate_k(number, array):
+  for index_one, i in enumerate(array):
+    for index_two, j in enumerate(array):
+      if index_one >= index_two:
+        continue
+      else:
+        if i + j == k:
+          return True
+  return False
+
+# one pass
+def pro_evaluate_k(number, array):
+  for index, i in enumerate(array):
+    if index == array.index(number):
+      continue
+  return False
+
+
+
+print(evaluate_k(k, numbers))
+print(pro_evaluate_k(k, numbers))
